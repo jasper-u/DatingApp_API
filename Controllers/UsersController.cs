2d393d444ae252aa151189bcf57a64aeeacec15f
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2023-05-15 18:50:27 +0200

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp_API.Data;
+using DatingApp_API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DatingApp_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users
+    public class UsersController
+    {
+        private readonly MainDbContext _context;
+
+        public UsersController(MainDbContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+            
+        }
+    }
+}
---
+++ Data/MainDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DatingApp_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp_API.Data
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
